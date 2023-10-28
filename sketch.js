fba931e66d01b24c209033d5e0bff7c8630e5994
t940119 <144977109+t940119@users.noreply.github.com>
2023-10-29 06:53:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://t940119.github.io/computerai/';
+let imageModelURL = 'https://t940119.github.io/computer.ai/';
 
 // Video
 let video;
